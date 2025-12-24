dc15e4cbae9df3acbc3cd06003fc41121dd776aa
keanecxz <chanxz.keane@gmail.com>
2025-12-24 20:29:05 +0800

    feat: Add GitHub Pages deployment configuration and scripts.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// If your repo is https://github.com/<user>/<repo>
+// then base must be "/<repo>/"
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/christmas-card/",
+});
